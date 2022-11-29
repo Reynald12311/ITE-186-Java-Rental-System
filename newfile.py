5cdc0b829cdb0f596fe78f5a9114f2decccb96bc
Reynald12311 <118160763+Reynald12311@users.noreply.github.com>
2022-11-29 13:19:46 +0800

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,123 @@
+player1=input("Enter your name:")
+colors=input("Guess the color:")
+if colors=='Blue':
+	print("Wrong")
+elif colors=='Green':
+	print("Correct")
+elif colors=='Violet':
+	print("Wrong")
+elif colors=='Black':
+	print("Wrong")
+elif colors=='White':
+	print("Wrong")
+elif colors=='Brown':
+	print("Wrong")
+elif colors=='Red':
+	print("Wrong")
+elif colors=='Orange':
+	print("Wrong")
+elif colors=='Yellow':
+	print("Wrong")
+elif colors=='Gray':
+	print("Wrong")
+else:("Error")
+#———————————————————————
+
+player2=input("Enter your name:")
+colors=input("Guess the color:")
+if colors=='Blue':
+	print("Wrong")
+elif colors=='Green':
+	print("Wrong")
+elif colors=='Violet':
+	print("Wrong")
+elif colors=='Black':
+	print("Wrong")
+elif colors=='White':
+	print("Wrong")
+elif colors=='Brown':
+	print("Wrong")
+elif colors=='Red':
+	print("Wrong")
+elif colors=='Orange':
+	print("Wrong")
+elif colors=='Yellow':
+	print("Wrong")
+elif colors=='Gray':
+	print("Correct")
+else:("Error")	#———————————————————————
+
+player3=input("Enter your name:")
+colors=input("Guess the color:")
+if colors=='Blue':
+	print("Wrong")
+elif colors=='Green':
+	print("Wrong")
+elif colors=='Violet':
+	print("Wrong")
+elif colors=='Black':
+	print("Wrong")
+elif colors=='White':
+	print("Wrong")
+elif colors=='Brown':
+	print("Wrong")
+elif colors=='Red':
+	print("Wrong")
+elif colors=='Orange':
+	print("Correct")
+elif colors=='Yellow':
+	print("Wrong")
+elif colors=='Gray':
+	print("Wrong")
+else:("Error")	#———————————————————————
+
+player4=input("Enter your name:")
+colors=input("Guess the color:")
+if colors=='Blue':
+	print("Wrong")
+elif colors=='Green':
+	print("Wrong")
+elif colors=='Violet':
+	print("Wrong")
+elif colors=='Black':
+	print("Wrong")
+elif colors=='White':
+	print("Correct")
+elif colors=='Brown':
+	print("Wrong")
+elif colors=='Red':
+	print("Wrong")
+elif colors=='Orange':
+	print("Wrong")
+elif colors=='Yellow':
+	print("Wrong")
+elif colors=='Gray':
+	print("Wrong")
+else:("Error")	
+#———————————————————————
+
+player5=input("Enter your name:")
+colors=input("Guess the color:")
+if colors=='Blue':
+	print("Wrong")
+elif colors=='Green':
+	print("Wrong")
+elif colors=='Violet':
+	print("Correct")
+elif colors=='Black':
+	print("Wrong")
+elif colors=='White':
+	print("Wrong")
+elif colors=='Brown':
+	print("Wrong")
+elif colors=='Red':
+	print("Wrong")
+elif colors=='Orange':
+	print("Wrong")
+elif colors=='Yellow':
+	print("Wrong")
+elif colors=='Gray':
+	print("Wrong")
+else:("Error")                 
+
+
